1f3735ab6e27fe21becfd286ba67dcdbbf431deb
kaloyanski <kaloyan@triplehelix-consulting.com>
2023-10-20 14:34:45 +0300

    add

---
+++ docs/metafetch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# metafetch.sh
+# fetch rpi metadata
+error() { echo error: $1; exit 111; }
+if [ ! $# -eq 1 ]; then error "usage: $0 <directory>"; fi
+if [ ! -d $1 ]; then error "$1 not a directory"; fi
+
+echo fetching metadata in $1 ...
+git clone -b kirkstone \
+    git@github.com:yoctoproject/poky.git $1/poky
+git clone -b kirkstone \
+    git@github.com:openembedded/meta-openembedded.git $1/oe
+git clone -b kirkstone \
+    git@github.com:agherzan/meta-raspberrypi $1/rpi/meta-raspberrypi
+git clone \
+    git@github.com:kaloyanski/meta-thc.git $1/thc/meta-thc
+
+exit 0
