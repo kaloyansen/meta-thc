23647e0e941e98ab2d968a0c11487cb10a02b369
kaloyansen <kalojanssen@gmail.com>
2023-10-21 11:05:27 +0300

    auto-commit documentation

--- docs/metafetch.sh
+++ docs/metafetch.sh
@@ -1,34 +1,33 @@
 #!/usr/bin/env sh
 # metafetch.sh
-# fetch rpi metadata
+# fetch rpi metadata branch
 
-if [ ! "$#" -eq 2 ]; then echo "usage: $0 <layerdir> <buildir>"; exit 112; fi
+if [ ! "$#" -eq 2 ]; then echo "usage: $0 <layerdir> <buildir>"; exit 112;
+fi
 
 nodir() {
-    echo "error: $1 is not a directory";
-    mkdir $1 && echo "info: directory $1 created" || exit 111;
+    echo error: $1 is not a directory && mkdir $1 &&
+        echo info: directory $1 created ||
+            exit 111;
 }
 
 [ -d $1 ] || nodir $1
 [ -d $2 ] || nodir $2
 
-echo "fetching metadata in $1 ..."
-echo "fetching configuration in $2 ..."
+BRANCH=kirkstone
+LAYER=$(realpath $1) && echo fetching $BRANCH in $LAYER
+BUILD=$(realpath $2) && echo fetching configuration in $BUILD
 
-git clone -b kirkstone \
-    git@github.com:yoctoproject/poky.git \
-    $1/poky
-git clone -b kirkstone \
-    git@github.com:openembedded/meta-openembedded.git \
-    $1/oe
-git clone -b kirkstone \
-    git@github.com:agherzan/meta-raspberrypi \
-    $1/rpi/meta-raspberrypi
-git clone \
-    git@github.com:kaloyanski/meta-thc.git \
-    $1/thc/meta-thc
-git clone \
-    git@github.com:TripleHelixConsulting/rpiconf.git \
-    $2/conf
+git clone -b $BRANCH git@github.com:yoctoproject/poky.git $LAYER/poky
+git clone -b $BRANCH git@github.com:openembedded/meta-openembedded.git $LAYER/oe
+git clone -b $BRANCH git@github.com:agherzan/meta-raspberrypi $LAYER/rpi/meta-raspberrypi
+git clone git@github.com:kaloyanski/meta-thc.git $LAYER/thc/meta-thc
+git clone git@github.com:TripleHelixConsulting/rpiconf.git $BUILD/conf
+
+echo sed $LAYER
+sed --debug -i s#/home/yocto/layer#$LAYER#g $BUILD/conf/bblayers.conf
+source $LAYER/poky/oe-init-build-env $BUILD
+bitbake-layers show-layers
+# bitbake core-image-x11
 
 exit 0
