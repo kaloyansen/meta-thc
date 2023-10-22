07c207505befb5c163dbf1156da15487753ca919
kaloyansen <kalojanssen@gmail.com>
2023-10-22 07:58:25 +0300

    auto-commit documentation

--- docs/metafetch.sh
+++ docs/metafetch.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/busybox sh
 # metafetch.sh
 # fetch rpi metadata
 
+FETCHER=git@github.com
 BRANCH=kirkstone
 
 while getopts ":l:b:r:h" option; do
@@ -14,29 +15,36 @@ while getopts ":l:b:r:h" option; do
    esac
 done
 
-nodat() { echo $1; exit 111; }
+nodat() { echo $*; exit 111; }
 nodir() {
     echo error: $1 is not a directory && mkdir $1 &&
         echo info: directory $1 created ||
             exit 111;
 }
 
-[ -n "$LAYER" ] || nodat 'specify layer directory'
-[ -n "$BUILD" ] || nodat 'specify build directory'
+[ -n "$LAYER" ] || nodat specify layer directory
+[ -n "$BUILD" ] || nodat specify build directory
 [ -d $LAYER ] || nodir $LAYER
 [ -d $BUILD ] || nodir $BUILD
 
-LAYER=$(realpath $LAYER) && echo fetching $BRANCH in $LAYER
-BUILD=$(realpath $BUILD) && echo fetching configuration in $BUILD
+LAYER=$(realpath $LAYER) && echo $FETCHER $BRANCH in $LAYER
+BUILD=$(realpath $BUILD) && echo $FETCHER configuration in $BUILD
 
-git clone -b $BRANCH git@github.com:yoctoproject/poky.git $LAYER/poky
-git clone -b $BRANCH git@github.com:openembedded/meta-openembedded.git $LAYER/oe
-git clone -b $BRANCH git@github.com:agherzan/meta-raspberrypi $LAYER/rpi/meta-raspberrypi
-git clone git@github.com:kaloyanski/meta-thc.git $LAYER/thc/meta-thc
-git clone git@github.com:TripleHelixConsulting/rpiconf.git $BUILD/conf
+read -p "confirm (y/n) " choice
+case "$choice" in
+    y ) echo $FETCHER;;
+    * ) echo refused; rmdir -v $LAYER $BUILD; exit 0;;
+esac
+
+git clone -b $BRANCH $FETCHER:yoctoproject/poky.git $LAYER/poky
+git clone -b $BRANCH $FETCHER:openembedded/meta-openembedded.git $LAYER/oe
+git clone -b $BRANCH $FETCHER:agherzan/meta-raspberrypi $LAYER/rpi/meta-raspberrypi
+git clone $FETCHER:kaloyanski/meta-thc.git $LAYER/thc/meta-thc
+git clone $FETCHER:TripleHelixConsulting/rpiconf.git $BUILD/conf
 
 sed -i s#/home/yocto/layer#$LAYER#g $BUILD/conf/bblayers.conf
 source $LAYER/poky/oe-init-build-env $BUILD
+
 bitbake-layers show-layers
 # bitbake core-image-x11
 
