f494dbba8e933ce70bbf458ca6c7b53b11051253
kaloyanski <kaloyan@triplehelix-consulting.com>
2023-10-23 13:29:48 +0300

    auto-commit by Kaloyan KRASTEV

--- recipes-copy/thcp/data/wifunited.sh
+++ recipes-copy/thcp/data/wifunited.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # wifini.sh
-# initialize wifi
+# wifi connection
+# requirements:
+# wpa_passphrase wpa_supplicant, ip, iw, grep, awk 
 # contact kaloyansen at gmail dot com
 # copyleft triplehelix-consulting.com
 # # # # # # # # # # # # # # # # # # # # # # # #
@@ -9,6 +11,7 @@ WINTERFACE=$(iw dev|grep Interface|awk '{print $2}')
 SSID=PuzlCowOrKing
 WPACONF=/etc/wpa_supplicant.conf
 WPASOCKET=/run/wpa_supplicant/$WINTERFACE
+UDHCPID=/run/udhcpc.$WINTERFACE.pid
 
 error() { echo $*; exit 1; }
 
@@ -21,27 +24,19 @@ while getopts ":i:s:h" option; do
    esac
 done
 
-ip link set $WINTERFACE up
-ip link show $WINTERFACE
 
-SSIS=$(iw $WINTERFACE scan|grep $SSID)
-[ -n "$SSIS" ] || error warning: cannot find network $SSID;
-
-SSPASS=$(grep $SSID $WPACONF)
-[ -n "$SSPASS" ] || wpa_passphrase $SSID >> $WPACONF
-
+ip link show $WINTERFACE | grep UP || ip link set $WINTERFACE up
+iw $WINTERFACE scan|grep $SSID || error warning: cannot find network $SSID;
+grep $SSID $WPACONF || wpa_passphrase $SSID >> $WPACONF
 [ -S "$WPASOCKET" ] || wpa_supplicant -B -D wext -i $WINTERFACE -c $WPACONF
-
+# [ -f "$UDHCPID" ] ||
 udhcpc -i $WINTERFACE || error $?
 
-echo interface: $WINTERFACE \
-     ssid: $SSID \
-     ssis: $SSIS \
-     ssapass: $SSPASS \
-     wpasocket: $WPASOCKET \
 
-# iw $winterface link
-# ip addr show $winterface
+
+# ip addr show $WINTERFACE
+echo interface $WINTERFACE SSID $SSID
+# iw $WINTERFACE link
 # ip route show
 
 
