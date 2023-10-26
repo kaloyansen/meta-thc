be2a571153f2898f393049c3971d72d9112279df
kaloyanski <kaloyan@triplehelix-consulting.com>
2023-10-26 13:59:20 +0300

    s

--- recipes-copy/thcp/data/wifini.sh
+++ recipes-copy/thcp/data/wifini.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+#!/bin/ash
 # wifini.sh
 # wifi connection requirements:
 # wpa_passphrase, wpa_supplicant, ip, iw, grep, awk 
@@ -6,18 +6,29 @@
 # copyleft triplehelix-consulting.com
 # # # # # # # # # # # # # # # # # # # # # # # #
 
+# files
 WPACONF=/etc/wpa_supplicant.conf
 WPASOCKET=/run/wpa_supplicant/$WIFACE
 UDHCPID=/run/udhcpc.$WIFACE.pid
 IFCONF=/etc/network/interfaces
-IW=/usr/sbin/iw
-IP=/sbin/ip
-WPAPASS=/usr/bin/wpa_passphrase
-WPASUPP=/usr/sbin/wpa_supplicant
 
-WIFACE=$($IW dev|grep Interface|awk '{print $2}')
+# command-line tools
+WPAPASS=/usr/bin/wpa_passphrase
+IW=/usr/sbin/iw
+WPASUPP=/usr/sbin/wpa_supplicant
+DHCP=/sbin/udhcpc
+IP=/sbin/ip
+
+# wifi interface
+#WIFACE=$($IW dev|grep Interface|awk '{print $2}')
 
 erreur() { echo $* && exit 1; }
+
+bow=$(getopt p: $* | awk '{print $2}')
+echo $bow
+
+exit 0 || kill $$
+
 
 while getopts ":i:s:h" option; do
 
@@ -25,7 +36,7 @@ while getopts ":i:s:h" option; do
         i) WIFACE=$OPTARG;;
         s) SSID=$OPTARG;;
         h) erreur usage: $0 -s SSID;;
-        *) erreur $0: option unknown;;
+        *) erreur $0: option $opt unknown;;
     esac
 done
 
@@ -45,7 +56,7 @@ $IW dev|grep $SSID > /dev/null &&
 $IP link show $WIFACE | grep UP ||
     $IP link set $WIFACE up
 
-# search for wifi network
+# search for a wifi network
 $IW $WIFACE scan|grep $SSID ||
     erreur $0 warning: cannot find network $SSID;
 
@@ -56,8 +67,9 @@ grep $SSID $WPACONF ||
 # load network
 [ -S "$WPASOCKET" ] ||
     $WPASUPP -B -D wext -i $WIFACE -c $WPACONF
-# get ip address dhcp
-/sbin/udhcpc -i $WIFACE ||
+
+# start a dhcp client
+$DHCP -i $WIFACE ||
     erreur $0 warning: $?
 
 # [ -f "$UDHCPID" ] ||
