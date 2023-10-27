5306c8445dffa6833490b6fde5b871992b0131d9
kaloyanski <kaloyan@triplehelix-consulting.com>
2023-10-27 09:54:16 +0300

    auto-commit by Kaloyan KRASTEV

--- recipes-copy/thcp/data/wifini.sh
+++ recipes-copy/thcp/data/wifini.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 # wifini.sh
 # wifi connection requirements:
+
 # wpa_passphrase, wpa_supplicant, ip, iw, grep, awk 
 # designed by kaloyansen at gmail dot com
 # copyleft triplehelix-consulting.com
@@ -19,31 +20,23 @@ WPASUPP=/usr/sbin/wpa_supplicant
 DHCP=/sbin/udhcpc
 IP=/sbin/ip
 
-# wifi interface
-WIFACE=$($IW dev|grep Interface|awk '{print $2}')
-
 erreur() { echo $* && exit 1; }
 
-bow=$(getopt p: $* | awk '{print $2}')
-echo $bow
+# get wifi interface and network id
+WIFACE=`$IW dev|grep Interface|awk '{print $2}'`
+SSID=$(getopt s: $* | awk '{print $2}')
+[ -n "$SSID" ] &&
+    echo $0: $SSID ||
+        erreur $0: specify SSID
+
+# enable interface connexion on boot
+[ -f $IFCONF ] &&
+    [[ grep "auto $WIFACE" $IFCONF > /dev/null &&
+        printf "auto $WIFACE\n" >> $IFCONF ]] ||
+        erreur $0: $IFCONF not found
 
 exit 0 || kill $$
 
-
-while getopts ":i:s:h" option; do
-
-    case $option in
-        i) WIFACE=$OPTARG;;
-        s) SSID=$OPTARG;;
-        h) erreur usage: $0 -s SSID;;
-        *) erreur $0: option $opt unknown;;
-    esac
-done
-
-[ -n "$SSID" ] ||
-    erreur $0: specify SSID
-
-# enable interface connexion on boot
 grep "auto $WIFACE" $IFCONF > /dev/null ||
     printf "auto $WIFACE\n" >> $IFCONF
 
@@ -56,7 +49,7 @@ $IW dev|grep $SSID > /dev/null &&
 $IP link show $WIFACE | grep UP ||
     $IP link set $WIFACE up
 
-# search for a wifi network
+# search network
 $IW $WIFACE scan|grep $SSID ||
     erreur $0 warning: cannot find network $SSID;
 
