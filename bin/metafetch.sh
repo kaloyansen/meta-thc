b34ebc04d53f322da3f1d095e5fc5ae54445099f
kaloyanski <kaloyan@triplehelix-consulting.com>
2023-11-07 13:06:03 +0200

    auto-commit by Kaloyan KRASTEV

--- bin/metafetch.sh
+++ bin/metafetch.sh
@@ -6,8 +6,8 @@
 FETCHER=https://github.com/
 GITFETCHER=git@github.com:
 BRANCH=kirkstone
-DEFLAYER=$HOME/yocto_$BRANCH/metadata
-DEFBUILD=$HOME/yocto_$BRANCH/rpi4
+DEFMETADIR=$HOME/yocto_$BRANCH/metadata
+DEFBUILDIR=$HOME/yocto_$BRANCH/rpi4
 
 erreur() { echo $* && exit 0 || kill $$; }
 
@@ -21,8 +21,8 @@ usage:
     -d            \t dry run                 \t wet run
     -g            \t switch to git protocol  \t https protocol
     -r <branch>   \t branch                  \t $BRANCH
-    -l <layerdir> \t metadata directory      \t $DEFLAYER
-    -b <buildir>  \t build directory         \t $DEFBUILD
+    -m <metadir>  \t metadata directory      \t $DEFMETADIR
+    -b <buildir>  \t build directory         \t $DEFBUILDIR
 "
     erreur
 }
@@ -35,12 +35,12 @@ confirm() {    # get confirmation or quit
             erreur $1 interrupted
 }
 
-while getopts ":l:b:r:hgd" option; do    # parce command-line options
+while getopts ":m:b:r:hgd" option; do    # parce command-line options
 
     case $option in
 
-        l ) LAYER=$OPTARG;;
-        b ) BUILD=$OPTARG;;
+        m ) METADIR=$OPTARG;;
+        b ) BUILDIR=$OPTARG;;
         r ) BRANCH=$OPTARG;;
         g ) FETCHER=$GITFETCHER;;
         d ) DRYRUN=yes;;
@@ -50,21 +50,21 @@ while getopts ":l:b:r:hgd" option; do    # parce command-line options
 done
 
 # check system path
-[ -n "$LAYER" ] || LAYER=$DEFLAYER
-[ -n "$BUILD" ] || BUILD=$DEFBUILD
-[ -d $LAYER ] || mkdir -p $LAYER || erreur $? cannot create $LAYER
-[ -d $BUILD ] || mkdir -p $BUILD || erreur $? cannot create $BUILD
-LAYER=$(realpath $LAYER) && printf "\nmetadata:\t $LAYER\n" || erreur $? cannot find $LAYER
-BUILD=$(realpath $BUILD) && printf "build:\t\t $BUILD\n" || erreur $? cannot find $BUILD
+[ -n "$METADIR" ] || METADIR=$DEFMETADIR
+[ -n "$BUILDIR" ] || BUILDIR=$DEFBUILDIR
+[ -d $METADIR ] || mkdir -p $METADIR || erreur $? cannot create $METADIR
+[ -d $BUILDIR ] || mkdir -p $BUILDIR || erreur $? cannot create $BUILDIR
+METADIR=$(realpath $METADIR) && printf "\nmetadata:\t $METADIR\n" || erreur $? cannot find $METADIR
+BUILDIR=$(realpath $BUILDIR) && printf "build:\t\t $BUILDIR\n" || erreur $? cannot find $BUILDIR
 printf "branch:\t\t $BRANCH\nprotocol:\t $FETCHER\n\n"
 
 declare -A REPO
 REPO=(    # associative array of git repositories
-    [yoctoproject/poky.git]=$LAYER/poky
-    [openembedded/meta-openembedded.git]=$LAYER/oe
-    [agherzan/meta-raspberrypi]=$LAYER/rpi/meta-raspberrypi
-    [kaloyanski/meta-thc.git]=$LAYER/thc/meta-thc
-    [TripleHelixConsulting/rpiconf.git]=$BUILD/conf
+    [yoctoproject/poky.git]=$METADIR/poky
+    [openembedded/meta-openembedded.git]=$METADIR/oe
+    [agherzan/meta-raspberrypi]=$METADIR/rpi/meta-raspberrypi
+    [kaloyanski/meta-thc.git]=$METADIR/thc/meta-thc
+    [TripleHelixConsulting/rpiconf.git]=$BUILDIR/conf
 )
 
 [ -n "$DRYRUN" ] || confirm $0 confirmation
@@ -78,18 +78,18 @@ done
 [ -n "$DRYRUN" ] && erreur $0 dry run exit
 
 # adjust bibtbake layer configuration
-sed -i s#/home/yocto/layer#$LAYER#g $BUILD/conf/bblayers.conf || erreur sed $?
+sed -i s#/home/yocto/layer#$METADIR#g $BUILDIR/conf/bblayers.conf || erreur sed $?
 
 # bitbake environment
 OEINIT=oe-init-build-env
-cd $LAYER/poky && pwd || erreur $? cannot find $LAYER/poky
-[ -f $OEINIT ] && . ./$OEINIT $BUILD || erreur $? cannot find $OEINIT
+cd $METADIR/poky && pwd || erreur $? cannot find $METADIR/poky
+[ -f $OEINIT ] && . ./$OEINIT $BUILDIR || erreur $? cannot find $OEINIT
 
 bitbake-layers show-layers
 
 printf "\n\t === how to start a new build === \n\n"
 
-echo cd $LAYER/poky
-echo . ./$OEINIT $BUILD
+echo cd $METADIR/poky
+echo . ./$OEINIT $BUILDIR
 echo bitbake core-image-x11
 echo
